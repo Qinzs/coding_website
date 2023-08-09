c2c77a6d5f1907e3582672d963aee71e3fa4a8a9
zsqin <1597868182@qq.com>
2023-08-09 15:18:40 +1000

    finish init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>{{user}}</h1>
+    <h1>{{pass}}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from 'vue'
+import $ from 'jquery'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup: () => {
+    let user = ref('')
+    let pass = ref('')
+      $.ajax({
+        url: 'http://127.0.0.1:3000/pk/user',
+        type: 'GET',
+        success: function (data) {
+          user.value = data.name
+          pass.value = data.email
+        }
+      })
+  
+  return {
+    user,
+    pass
   }
+}
+
 }
 </script>
