9ce3e5c0bce06c1b5945b9fdf5da0aed2380e269
zsqin <1597868182@qq.com>
2023-08-09 17:00:32 +1000

    use typescript

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
+import 'element-plus/dist/index.css'
+const app = createApp(App)
+
+
+app.use(ElementPlus)
+
+
 
 createApp(App).use(router).mount('#app')
+
+
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1>{{user}}</h1>
-    <h1>{{pass}}</h1>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import { ref } from 'vue'
-import $ from 'jquery'
-export default {
-  name: 'HomeView',
-  setup: () => {
-    let user = ref('')
-    let pass = ref('')
-      $.ajax({
-        url: 'http://127.0.0.1:3000/pk/user',
-        type: 'GET',
-        success: function (data) {
-          user.value = data.name
-          pass.value = data.email
-          console.log(data)
-        }
-      })
-  
-  return {
-    user,
-    pass
-  }
-}
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-}
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {}
 </script>
